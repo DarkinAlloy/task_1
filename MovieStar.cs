e64c0a91202b78b1237e3356132f3eb030efb0cb
Nigger Hunter 1337 <Nigger Hunter 1337@DESKTOP-FFUSID1>
2022-07-21 14:17:38 +0300

    Add project files.

---
+++ MovieStar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    [Serializable]
+    internal class MovieStar
+    {
+        public string? dateOfBirth { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Sex { get; set; }
+        public string? Nationality { get; set; }
+
+        public int get_age(string dateOfBirth)
+        {
+            DateTime dob = Convert.ToDateTime(dateOfBirth);
+            int age = 0;
+            age = DateTime.Now.AddYears(-dob.Year).Year;
+            return age;
+        }
+        /*
+        public MovieStar(string aDateOfBirth, string aName, string aSurname, string aSex, string aNationality)
+        {
+            dateOfBirth = aDateOfBirth;
+            name = aName;
+            surname = aSurname;
+            sex = aSex;
+            nationality = aNationality;
+        }
+        */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var movieStars = name1();
+            if (movieStars != null)
+            {
+                //Console.WriteLine(movieStars);
+                for (int i = 0; i < movieStars.Count; i++)
+                {
+                    Console.WriteLine(movieStars[i].Name + " " + movieStars[i].Surname);
+                    Console.WriteLine(movieStars[i].Sex);
+                    Console.WriteLine(movieStars[i].Nationality);
+                    Console.WriteLine(movieStars[i].get_age(movieStars[i].dateOfBirth) + " years old");
+                    Console.WriteLine("");
+
+                }
+            }
+            else
+                return;
+        }
+
+        static List<MovieStar> name1()
+        {
+            string path = @"N:\input.txt"; //Location may vary - in my personal case, it was in that spot
+
+            if (File.Exists(path))
+            {
+                var movieStars = JsonConvert.DeserializeObject<List<MovieStar>>(File.ReadAllText(path));
+
+#pragma warning disable CS8603 // Possible null reference return.
+                return movieStars;
+#pragma warning restore CS8603 // Possible null reference return.
+            }
+            else
+                Console.WriteLine("File not found");
+                return null;
+        }
+    }
+}
